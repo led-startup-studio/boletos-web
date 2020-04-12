900fc65cca9d79c38e16defb735b1985ccac1ab4
Fabricio Godoi <fabricio@ledstartup.studio>
2020-04-12 02:01:07 -0300

    Updates

---
+++ precache-manifest.aaecb5cde79dff39af5d1ece99f8934c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f76ecf23cb046d5c041bfe8a3c3be3",
+    "url": "/boletos-web/index.html"
+  },
+  {
+    "revision": "d2cd4f685bc60598eaca",
+    "url": "/boletos-web/static/css/main.3f3241b6.chunk.css"
+  },
+  {
+    "revision": "5be28003e62faa2be69b",
+    "url": "/boletos-web/static/js/2.61c13646.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boletos-web/static/js/2.61c13646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cd4f685bc60598eaca",
+    "url": "/boletos-web/static/js/main.bab1d4f2.chunk.js"
+  },
+  {
+    "revision": "8f6ce283572d8903a12c",
+    "url": "/boletos-web/static/js/runtime-main.bac7cd8d.js"
+  },
+  {
+    "revision": "c418149f7c0b90cf0bf7fea1b24ab59f",
+    "url": "/boletos-web/static/media/logo.c418149f.png"
+  }
+]);
